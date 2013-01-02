49a3e34b9bc46136e8d7e31437f6095589574b93
Armin Burgmeier <armin@arbur.net>
2013-01-02 15:39:15 +0100

    Optimize k, mu, eta and m a bit

--- src/object/C4Rope.cpp
+++ src/object/C4Rope.cpp
@@ -774,7 +774,7 @@ bool C4RopeElement::SetForceRedirectionByLookAround(const C4Rope* rope, int ox, 
 
 C4Rope::C4Rope(C4PropList* Prototype, C4Object* first_obj, C4Object* second_obj, C4Real segment_length, C4DefGraphics* graphics):
 	C4PropListNumbered(Prototype), Width(5.0f), Graphics(graphics), SegmentCount(fixtoi(ObjectDistance(first_obj, second_obj)/segment_length)),
-	l(segment_length), k(Fix1*3), mu(Fix1*3), eta(Fix1*3), NumIterations(100),
+	l(segment_length), k(Fix1*20), mu(Fix1*3), eta(Fix1*4), NumIterations(10),
 	FrontAutoSegmentation(Fix0), BackAutoSegmentation(Fix0), FrontPull(Fix0), BackPull(Fix0)
 {
 	if(!PathFree(first_obj->GetX(), first_obj->GetY(), second_obj->GetX(), second_obj->GetY()))
@@ -784,8 +784,8 @@ C4Rope::C4Rope(C4PropList* Prototype, C4Object* first_obj, C4Object* second_obj,
 
 	Front = new C4RopeElement(first_obj, false);
 	Back = new C4RopeElement(second_obj, false);
-
-	const C4Real m(Fix1*1); // TODO: This should be a property
+	
+	const C4Real m(Fix1*5); // TODO: This should be a property
 
 	C4RopeElement* prev_seg = Front;
 	for(int32_t i = 0; i < SegmentCount; ++i)
